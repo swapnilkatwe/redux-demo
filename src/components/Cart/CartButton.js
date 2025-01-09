025fc212f9cb957c1bc8a69207f96400fb583019
Swapnil Katwe <swapnil.katwe111@gmail.com>
2025-01-09 16:08:41 +0100

    store integrated with cart, products components. product can now add. Increment or decrement in cart using plus and minus button.

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,20 @@
 import classes from './CartButton.module.css';
+import {useDispatch} from "react-redux";
+import { uiActions } from '../../store/ui-slice';
+import {useSelector} from "react-redux";
 
 const CartButton = (props) => {
+  const cartQuantity = useSelector(state=>state.cart.totalQuantity);
+  const dispatch = useDispatch();
+
+  function toggleCartHandle() {
+    dispatch(uiActions.toggle());
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandle}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    title: "First product",
+    description: "This is my first product",
+    price: 200,
+  },
+  {
+    id: "p2",
+    title: "Second product",
+    description: "This is my second product",
+    price: 200,
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
+        {DUMMY_PRODUCTS.map(product => 
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+          key = {product.id}
+          id = {product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+        />)}
       </ul>
     </section>
   );
